7b8a7dcbd41a480c58f848ca5f75bd7a21966dfc
Aryzin <ary.m@aluno.ifsc.edu.br>
2022-09-05 18:14:15 -0300

    Atividade 2

---
+++ AT2/PessoaMAIN.java
@@ -0,0 +1,31 @@
+package at2;
+
+	import java.util.ArrayList;
+
+	public class pessoaMain {
+
+		public static void main(String[] args) {
+			ArrayList<Pessoa> pessoa = new ArrayList<>();
+			Pessoa p2 = new Pessoa();
+			p2.setNome("Ary");
+			Pessoa p3 = new Pessoa();
+			p3.setNome("Lais");
+			Pessoa p4 = new Pessoa();
+			p4.setNome("Irwyng");
+			Pessoa p5 = new Pessoa();
+			p5.setNome("Aldair");
+			Pessoa p6 = new Pessoa();
+			p6.setNome("Isabella");
+		
+			pessoa.add(p6);
+			pessoa.add(p5);
+			pessoa.add(p4);
+			pessoa.add(p3);
+			pessoa.add(p2);
+			
+			for (Pessoa pes : pessoa) {
+				System.out.println(pes.getNome());
+			}
+		}
+
+	}
